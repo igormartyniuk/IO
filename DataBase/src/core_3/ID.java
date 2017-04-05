40f3a830c06beb00cfa2c2d8c8bde9d259ade7d7
Kanibal <Kanibal@DESKTOP-J92RFN7>
2017-04-05 17:22:46 +0300

    (

---
+++ DataBase/src/core_3/ID.java
@@ -0,0 +1,25 @@
+package core_3;
+
+public class ID {
+
+	private int idNumber;
+
+	public ID(int idNumber) {
+		super();
+		this.idNumber = idNumber;
+	}
+
+	public int getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(int idNumber) {
+		this.idNumber = idNumber;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ DataBase/src/core_3/Main.java
@@ -0,0 +1,34 @@
+package core_3;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+	static	Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+		DB db = new DB();
+	
+//		db.addPerson();
+//		db.removePerson();
+//		db.showDataBase();
+//		db.updateDataBase();
+//		db.findByIdNumber();
+		
+		
+		
+		db.menu();
+		
+		
+		
+		
+		
+		
+	
+	}
+
+
+}
